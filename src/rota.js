c875ee21e15ded929d882e7ef48c486944c81515
Ivonildo31 <39799519+Ivonildo31@users.noreply.github.com>
2018-12-29 11:13:57 -0200

    Update rota.js

--- src/rota.js
+++ src/rota.js
@@ -1,22 +1,25 @@
 var http= require('http');
 
- var criarRota=function(port){
-     var api ={};
-     var routes={};
-     var methods=['GET','POST'];
+ var createRouter=function(port){
+     var api     = {};
+     var routes  = {};
+     var methods = ['GET','POST'];
+
      methods.forEach(function(method){
-         routes[method]={};
-         api[method.toLowerCase()]=function(path,fn){
+         routes[method] = {};
+         api[method.toLowerCase()] = function(path,fn){
              routes[method][path]=fn;
          };
      });
 
-     http.criarServer(function(req,res){
-        res.serHeader('Access-Control-Alow-Origi','*');
-        if(!routes[req.method][req.url])return res.end();
+     http.createServer(function(req,res){
+        res.setHeader('Access-Control-Allow-Origin','*');
+        if(!routes[req.method][req.url]) return res.end();
+        routes[req.method][req.url](req,res);
         
      }).listen(port);
-     return api;
- }   
 
- module.exports = criarRota;
+     return api;
+ } ;  
+
+ module.exports = createRouter;
